0d525483de74a7e54def3ede425120c66946d6c8
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-06 18:07:27 +0800

    injected character service into the character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,6 +10,7 @@ namespace dotnet_rpg.Controllers
     [Route("api/v1/[controller]")]
     public class CharacterController : ControllerBase
     {
+
         static readonly List<Character> characters = new()
         {
             new(),
@@ -18,24 +19,30 @@ namespace dotnet_rpg.Controllers
                 Class = RpgClass.Mage
             }
         };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
         [HttpGet]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(character => character.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public ActionResult<List<Character>> CreateCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.CreateCharacter(newCharacter));
         }
     }
 }
