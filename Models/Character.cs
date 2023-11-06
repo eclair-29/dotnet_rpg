75aa9adb3ce999ccf0f5194262d881df397ce295
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-06 11:51:55 +0800

    create initial rpg model

---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Traveller";
+        public int HealthPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Agility { get; set; } = 15;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
