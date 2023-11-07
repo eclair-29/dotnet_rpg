28ba395ff8c3f599809d0e5d2502168b2f55eccc
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-07 15:28:53 +0800

    register auto mapper package

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 global using dotnet_rpg.Models;
 global using dotnet_rpg.Services;
 global using dotnet_rpg.Dtos.Character;
+global using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
