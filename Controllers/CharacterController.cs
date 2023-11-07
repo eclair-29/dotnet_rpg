24e9c0b9cccba85bdb6188b1add4da6afe95bfc9
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-07 10:11:10 +0800

    set DTO model for get and add

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -27,19 +27,19 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> CreateCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> CreateCharacter(AddCharacterDto newCharacter)
         {
             characters.Add(newCharacter);
 
