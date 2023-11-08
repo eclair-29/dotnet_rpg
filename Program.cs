68f32c7bd39587a9f2b03c8d1ebd10978c3039e2
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2023-11-08 16:37:03 +0800

    register db data context with connection string

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@ global using dotnet_rpg.Models;
 global using dotnet_rpg.Services;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
